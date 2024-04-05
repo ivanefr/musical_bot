c2772c145dfac44f40535b27d1a617d25d0df952
ivanefr <Efrulka2009@gmail.com>
2024-04-05 19:23:17 +0300

    fix shazam.py

--- shazam.py
+++ shazam.py
@@ -4,7 +4,6 @@ from shazamio import Shazam, Serialize
 class Song:
     def __init__(self, data):
         self.data = data
-        self.file_path = "voice.mp3"
         self.__shazam = Shazam(language="ru-RU")
         self.__out = None
         self.__artist = None
